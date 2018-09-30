2f217c56f2fd32ecfc0b39150b90dd26744963c8
Olli Huotari <olli.huotari@iki.fi>
2018-09-30 17:12:03 +0300

    some refactorings

--- yolov3_pytorch/yolov3.py
+++ yolov3_pytorch/yolov3.py
@@ -1,4 +1,3 @@
-
 from collections import OrderedDict, Iterable, defaultdict
 import torch
 import torch.nn as nn
@@ -8,55 +7,7 @@ import importlib
 from .yolo_layer import *
 from .yolov3_base import *
 
-class Darknet(nn.Module):
-    def __init__(self, num_blocks, start_nf=32):
-        super().__init__()
-        nf = start_nf
-        self.base = ConvBN(3, nf, kernel_size=3, stride=1) #, padding=1)
-        self.layers = []
-        for i, nb in enumerate(num_blocks):
-            # dn_layer = make_group_layer(nf, nb, stride=(1 if i==-1 else 2))
-            dn_layer = self.make_group_layer(nf, nb, stride=2)
-            self.add_module(f"darknet_{i}", dn_layer)
-            self.layers.append(dn_layer)
-            nf *= 2
-
-    def make_group_layer(self, ch_in, num_blocks, stride=2):
-        layers = [ConvBN(ch_in, ch_in*2, stride=stride)]
-        for i in range(num_blocks): layers.append(DarknetBlock(ch_in*2))
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        y = [self.base(x)]
-        for l in self.layers:
-            y.append(l(y[-1]))
-        return y
-
-class Yolov3UpsamplePrep(nn.Module):
-    def __init__(self, filters_list, in_filters, out_filters):
-        super().__init__()
-        self.branch = nn.ModuleList([
-                        ConvBN(in_filters, filters_list[0], 1),
-                        ConvBN(filters_list[0], filters_list[1], kernel_size=3),
-                        ConvBN(filters_list[1], filters_list[0], kernel_size=1),
-                        ConvBN(filters_list[0], filters_list[1], kernel_size=3),
-                        ConvBN(filters_list[1], filters_list[0], kernel_size=1),])
-        self.for_yolo = nn.ModuleList([
-                        ConvBN(filters_list[0], filters_list[1], kernel_size=3),
-                        nn.Conv2d(filters_list[1], out_filters, kernel_size=1, stride=1,
-                                   padding=0, bias=True)])
-#         self.upsample = upsample
-        
-    def forward(self, x):
-        for m in self.branch: x = m(x)
-        branch_out = x
-        for m in self.for_yolo: x = m(x)
-#         if self.upsample:
-#             x = nn.Upsample(scale_factor=2, mode='nearest')(x)
-        #return namedtuple('out', ['branch', 'for_yolo'])(branch_out, x)
-        return branch_out, x
-
-class Yolov3(nn.Module):
+class Yolov3(Yolov3Base):
     def __init__(self, num_classes=80):
         super().__init__()
         self.backbone = Darknet([1,2,8,8,4])
@@ -109,23 +60,79 @@ class Yolov3(nn.Module):
 
 
 
-    def boxes_from_output(self, outputs, conf_thresh=0.25):
-        all_boxes = [[] for j in range(outputs[0].size(0))]
-        for i, layer in enumerate(self.get_loss_layers()):
-            layer_boxes = layer.get_region_boxes(outputs[i], conf_thresh=conf_thresh)
-            for j, layer_box in enumerate(layer_boxes):
-                all_boxes[j] += layer_box
+    # def boxes_from_output(self, outputs, conf_thresh=0.25):
+    #     all_boxes = [[] for j in range(outputs[0].size(0))]
+    #     for i, layer in enumerate(self.get_loss_layers()):
+    #         layer_boxes = layer.get_region_boxes(outputs[i], conf_thresh=conf_thresh)
+    #         for j, layer_box in enumerate(layer_boxes):
+    #             all_boxes[j] += layer_box
 
-        return all_boxes
+    #     return all_boxes
 
 
-    def predict_img(self, imgs, conf_thresh=0.25):
-        self.eval()
-        if len(imgs.shape) == 3: imgs = imgs.unsqueeze(-1) 
+    # def predict_img(self, imgs, conf_thresh=0.25):
+    #     self.eval()
+    #     if len(imgs.shape) == 3: imgs = imgs.unsqueeze(-1) 
         
-        outputs = self.forward(imgs)
-        return self.boxes_from_output(outputs, conf_thresh)
+    #     outputs = self.forward(imgs)
+    #     return self.boxes_from_output(outputs, conf_thresh)
 
 
+###################################################################
+## Backbone and helper modules
 
+class DarknetBlock(nn.Module):
+    def __init__(self, ch_in):
+        super().__init__()
+        ch_hid = ch_in//2
+        self.conv1 = ConvBN(ch_in, ch_hid, kernel_size=1, stride=1, padding=0)
+        self.conv2 = ConvBN(ch_hid, ch_in, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x): return self.conv2(self.conv1(x)) + x
+
+
+class Darknet(nn.Module):
+    def __init__(self, num_blocks, start_nf=32):
+        super().__init__()
+        nf = start_nf
+        self.base = ConvBN(3, nf, kernel_size=3, stride=1) #, padding=1)
+        self.layers = []
+        for i, nb in enumerate(num_blocks):
+            # dn_layer = make_group_layer(nf, nb, stride=(1 if i==-1 else 2))
+            dn_layer = self.make_group_layer(nf, nb, stride=2)
+            self.add_module(f"darknet_{i}", dn_layer)
+            self.layers.append(dn_layer)
+            nf *= 2
+
+    def make_group_layer(self, ch_in, num_blocks, stride=2):
+        layers = [ConvBN(ch_in, ch_in*2, stride=stride)]
+        for i in range(num_blocks): layers.append(DarknetBlock(ch_in*2))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        y = [self.base(x)]
+        for l in self.layers:
+            y.append(l(y[-1]))
+        return y
+
+
+class Yolov3UpsamplePrep(nn.Module):
+    def __init__(self, filters_list, in_filters, out_filters):
+        super().__init__()
+        self.branch = nn.ModuleList([
+                        ConvBN(in_filters, filters_list[0], 1),
+                        ConvBN(filters_list[0], filters_list[1], kernel_size=3),
+                        ConvBN(filters_list[1], filters_list[0], kernel_size=1),
+                        ConvBN(filters_list[0], filters_list[1], kernel_size=3),
+                        ConvBN(filters_list[1], filters_list[0], kernel_size=1),])
+        self.for_yolo = nn.ModuleList([
+                        ConvBN(filters_list[0], filters_list[1], kernel_size=3),
+                        nn.Conv2d(filters_list[1], out_filters, kernel_size=1, stride=1,
+                                   padding=0, bias=True)])
+
+    def forward(self, x):
+        for m in self.branch: x = m(x)
+        branch_out = x
+        for m in self.for_yolo: x = m(x)
+        return branch_out, x
 
--- yolov3_pytorch/yolov3_tiny.py
+++ yolov3_pytorch/yolov3_tiny.py
@@ -1,4 +1,3 @@
-
 from collections import OrderedDict, Iterable, defaultdict
 import torch
 import torch.nn as nn
@@ -9,55 +8,16 @@ from .yolo_layer import *
 from .yolov3_base import *
 
 
-class SplittedBackbone(nn.Module):
-    def __init__(self, layers_list, id):   
-        super().__init__()
-        self.layers_list = layers_list
-        self.layers = nn.Sequential(self.layers_list)
-        self.id = id
-    
-    def forward(self, x):
-        x_b_0 = self.layers[:self.id](x)
-        # forward hook might be clearer https://youtu.be/xXXiC4YRGrQ?t=5401 
-        x_b_full = self.layers[self.id:](x_b_0)
-        return x_b_0, x_b_full
-
-
-class Yolov3Tiny(nn.Module):
-
-    def get_loss_layers(self):
-        return [self.yolo_0, self.yolo_1]
+class Yolov3Tiny(Yolov3Base):
 
     def __init__(self, num_classes, use_wrong_previous_anchors=False):
         super().__init__()
 
         self.num_classes = num_classes
-        # self.use_cuda = (device == 'cuda')
-        # self.device = torch.device(device)
         self.return_out_boxes = False
         self.skip_backbone = False
-        
-        input_channels = 3
-        
-        self.backbone_layer_list = OrderedDict([
-            ('0_convbatch',     ConvBN(input_channels, 16, 3, 1, 1)),
-            ('1_max',           nn.MaxPool2d(2, 2)),
-            ('2_convbatch',     ConvBN(16, 32, 3, 1, 1)),
-            ('3_max',           nn.MaxPool2d(2, 2)),
-            ('4_convbatch',     ConvBN(32, 64, 3, 1, 1)),
-            ('5_max',           nn.MaxPool2d(2, 2)),
-            ('6_convbatch',     ConvBN(64, 128, 3, 1, 1)),
-            ('7_max',           nn.MaxPool2d(2, 2)),
-            ('8_convbatch',     ConvBN(128, 256, 3, 1, 1)),
-            ('9_max',           nn.MaxPool2d(2, 2)),
-            # ('9_max',           nn.MaxPool2d(2, 2, ceil_mode=True)), 
-            ('10_convbatch',    ConvBN(256, 512, 3, 1, 1)),
-            ('11_max',          MaxPoolStride1()),
-            ('12_convbatch',    ConvBN(512, 1024, 3, 1, 1)),
-            ('13_convbatch',    ConvBN(1024, 256, 1, 1, 0)), # padding = kernel_size-1//2
-        ])
 
-        self.backbone = SplittedBackbone(self.backbone_layer_list, 9)
+        self.backbone = Yolov3TinyBackbone()
 
         anchors_per_region = 3
         self.yolo_0_pre = nn.Sequential(OrderedDict([
@@ -87,23 +47,25 @@ class Yolov3Tiny(nn.Module):
 
         self.yolo_1 = YoloLayer(anchors=yolo_1_anchors, stride=16.0, num_classes=num_classes)
 
+    def get_loss_layers(self):
+        return [self.yolo_0, self.yolo_1]
 
-    def boxes_from_output(self, outputs, conf_thresh=0.25):
-        all_boxes = [[] for j in range(outputs[0].size(0))]
-        for i, layer in enumerate(self.get_loss_layers()):
-            layer_boxes = layer.get_region_boxes(outputs[i], conf_thresh=conf_thresh)
-            for j, layer_box in enumerate(layer_boxes):
-                all_boxes[j] += layer_box
+    # def boxes_from_output(self, outputs, conf_thresh=0.25):
+    #     all_boxes = [[] for j in range(outputs[0].size(0))]
+    #     for i, layer in enumerate(self.get_loss_layers()):
+    #         layer_boxes = layer.get_region_boxes(outputs[i], conf_thresh=conf_thresh)
+    #         for j, layer_box in enumerate(layer_boxes):
+    #             all_boxes[j] += layer_box
 
-        return all_boxes
+    #     return all_boxes
 
 
-    def predict_img(self, imgs, conf_thresh=0.25):
-        self.eval()
-        if len(imgs.shape) == 3: imgs = imgs.unsqueeze(-1) 
+    # def predict_img(self, imgs, conf_thresh=0.25):
+    #     self.eval()
+    #     if len(imgs.shape) == 3: imgs = imgs.unsqueeze(-1) 
         
-        outputs = self.forward(imgs)
-        return self.boxes_from_output(outputs, conf_thresh)
+    #     outputs = self.forward(imgs)
+    #     return self.boxes_from_output(outputs, conf_thresh)
 
 
     def forward_yolo(self, x_b_0, x_b_full):
@@ -127,39 +89,77 @@ class Yolov3Tiny(nn.Module):
         return out
 
 
-    def freeze_backbone(self, requires_grad=False):
-        for _, p in self.backbone.named_parameters():
-            p.requires_grad = requires_grad
-    def unfreeze(self):
-        for _, p in self.named_parameters():
-            p.requires_grad = True
-    def freeze_info(self, print_all=False):
-        d = defaultdict(set)
-        for name, param in self.named_parameters():
-            if print_all:
-                print(f"{name}: {param.requires_grad}")
-            else:
-                d[name.split('.')[0]].add(param.requires_grad)
-        if not print_all:
-            for k,v in d.items():
-                print(k, ': ', v)
+    # def freeze_backbone(self, requires_grad=False):
+    #     for _, p in self.backbone.named_parameters():
+    #         p.requires_grad = requires_grad
+    # def unfreeze(self):
+    #     for _, p in self.named_parameters():
+    #         p.requires_grad = True
+    # def freeze_info(self, print_all=False):
+    #     d = defaultdict(set)
+    #     for name, param in self.named_parameters():
+    #         if print_all:
+    #             print(f"{name}: {param.requires_grad}")
+    #         else:
+    #             d[name.split('.')[0]].add(param.requires_grad)
+    #     if not print_all:
+    #         for k,v in d.items():
+    #             print(k, ': ', v)
         
 
-    def load_only_backbone(self, h5_path):
-        state_dict = torch.load(h5_path)
+    # def load_only_backbone(self, h5_path):
+    #     state_dict = torch.load(h5_path)
 
-        for k in list(state_dict.keys()):
-            if k.startswith(('yolo_0_pre.15', 'yolo_1_pre.20')):
-                del state_dict[k]
+    #     for k in list(state_dict.keys()):
+    #         if k.startswith(('yolo_0_pre.15', 'yolo_1_pre.20')):
+    #             del state_dict[k]
 
-        # Renaming some keys if needed for compatibility
-        # state_dict = type(state_dict_org)()
-        # for k_old in list(state_dict.keys()):
-        #     k_new = k_old.replace('backend', 'backbone')
-        #     state_dict[k_new] = state_dict_org[k_old]
+    #     # Renaming some keys if needed for compatibility
+    #     # state_dict = type(state_dict_org)()
+    #     # for k_old in list(state_dict.keys()):
+    #     #     k_new = k_old.replace('backend', 'backbone')
+    #     #     state_dict[k_new] = state_dict_org[k_old]
 
-        return self.load_state_dict(state_dict, strict=False)
+    #     return self.load_state_dict(state_dict, strict=False)
 
 
+###################################################################
+## Backbone and helper modules
 
+class MaxPoolStride1(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        x = F.max_pool2d(F.pad(x, (0,1,0,1), mode='replicate'), 2, stride=1)
+        return x
+
+
+class Yolov3TinyBackbone(nn.Module):
+    def __init__(self, input_channels=3):
+        super().__init__()
+        self.layers_list =  OrderedDict([
+            ('0_convbatch',     ConvBN(input_channels, 16, 3, 1, 1)),
+            ('1_max',           nn.MaxPool2d(2, 2)),
+            ('2_convbatch',     ConvBN(16, 32, 3, 1, 1)),
+            ('3_max',           nn.MaxPool2d(2, 2)),
+            ('4_convbatch',     ConvBN(32, 64, 3, 1, 1)),
+            ('5_max',           nn.MaxPool2d(2, 2)),
+            ('6_convbatch',     ConvBN(64, 128, 3, 1, 1)),
+            ('7_max',           nn.MaxPool2d(2, 2)),
+            ('8_convbatch',     ConvBN(128, 256, 3, 1, 1)),
+            ('9_max',           nn.MaxPool2d(2, 2)),
+            # ('9_max',           nn.MaxPool2d(2, 2, ceil_mode=True)), 
+            ('10_convbatch',    ConvBN(256, 512, 3, 1, 1)),
+            ('11_max',          MaxPoolStride1()),
+            ('12_convbatch',    ConvBN(512, 1024, 3, 1, 1)),
+            ('13_convbatch',    ConvBN(1024, 256, 1, 1, 0)), # padding = kernel_size-1//2
+        ])
+        self.layers = nn.Sequential(self.layers_list)
+        self.idx = 9
+    
+    def forward(self, x):
+        x_b_0 = self.layers[:self.idx](x)
+        x_b_full = self.layers[self.idx:](x_b_0)
+        return x_b_0, x_b_full
 
